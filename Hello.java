7d08c51fae6c5230f7a4c78acdd41c86a1e92a0e
Eduardo-Rubel <ercanavarro@minha.fag.edu.br>
2025-02-25 20:02:10 -0300

    Ola Mundo

--- Hello.java
+++ Hello.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
 
 public class Hello {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+    }
+    public void helloWorld() {
         System.out.println("Hello World");
+        
+    }
+    public void olamundo(){
+        System.out.println("Ola Mundo");
     }
 }
